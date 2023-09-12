d592d3cf5a674b16a878a792c6dc928cd8d6853c
Brian Nyaga <briannyagak@gmail.com>
2023-09-12 20:00:46 +0300

    fff

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void print_alphabet(void);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	printf("_putchar\n");
+}
