53d4ade81d5684f03c7d6726e283f62c06244c25
Brian Nyaga <briannyagak@gmail.com>
2023-10-06 08:43:44 +0300

    more corrections

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,17 +14,23 @@ int *array_range(int min, int max)
 	int i, size;
 
 	if (min > max)
+	{
 		return (NULL);
+	}
 
 	size = max - min + 1;
 
 	ptr = malloc(sizeof(int) * size);
 
 	if (ptr == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i <= size; i++)
+	{
 		ptr[i] = min++;
+	}
 
 	return (ptr);
 }
