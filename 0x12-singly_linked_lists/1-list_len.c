31f0347046a9bda0b9c055bad43b0f25d646ba77
Brian Nyaga <briannyagak@gmail.com>
2023-10-20 00:46:00 +0300

    push

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in a linked list
+ * @h: the pointer to the list_t list
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+ size_t m = 0;
+
+ while (h)
+ {
+ m++;
+ h = h->next;
+ }
+ return (m);
+}
+
